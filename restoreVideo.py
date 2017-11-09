24df6bcf5d09fe2c83a379c93911f9274ff7222d
Vlad Zat <vlad.zat@mydit.ie>
2017-11-09 23:42:11 +0000

    Add first 3 steps of structure

--- restoreVideo.py
+++ restoreVideo.py
@@ -11,6 +11,76 @@
 #   Restore videos by firstly performing some cleaning on each individual frame
 #   such as contrast stretching, denoising and sharpening. After that temporal
 #   processing is used to stabilise the image to minimise the jittering.
+#
+#   Structure:
+#   1. Reading the video
+#       1.1 Extract video metadata
+#           * The codec (fourcc), fps, width and height [1] of the video are
+#             needed to be able to write the video in its original form
+#           * Because most codecs are causing problems on Windows, the fourcc
+#             has been set to 'XVID'
+#       1.2 Append the frames to a list
+#   2. Stretch Contrast
+#       2.1 Extract luminance from frame
+#           * For colour images the luminance can be used to scale the intensity
+#       2.2 Find the minimum and maximum intensity of the image
+#           * To be able to stretch the histogram, the range of the insensities
+#             need to be known
+#       2.3 Scale the range from the initial intensities to 0 - 255
+#           * By applying the formula 255 * (In - Imin) / (Imax - Imin) the
+#             initial intensities of the frame are scaled to the whole range
+#             from 0 - 255
+#           * This function does not improve the frame very much if the minimum
+#             and maximum intensities of the frame are already close to 0 - 255
+#   3. Stabilise video
+#       3.1 Iterate through the frames in pairs
+#           * Stabilisation is done by finding the global movement between each
+#             pair of frames
+#           * The frames are updated at each step to provide a smooth stabilisation
+#       3.2 Blur frames
+#           * The images are blurred to remove most of the noise from the image
+#           * Median Blurring is used as it provides a good trade-off between
+#             speed and noise removal
+#       3.3 Get edges
+#           * The edges of the frame are extracted using Canny
+#           * The thresholds used for Canny are calculated based on Otsu's
+#             theshold that is beforehand
+#           * Values between 0.5 * Otsu's theshold and the value of the threshold
+#             provides good results for most images [2]
+#       3.4 Find the phase correlation between the two frames
+#           * Phase correlation is a way of determening an offset between two
+#             similar images
+#           * It provides faster and more precise results than
+#             using a feature detection algorithm
+#           * It returns a subpixel offset for the x and y coordinates
+#           * The OpenCV function requires the images to be in a specific format
+#             (CV_32FC1 or CV_64FC1) [3]
+#       3.5 Translate the second frame to match the first one
+#           * A transformation matrix is created using the values from the
+#             previous step [4]
+#           * By using the translation matrix, the second frame is shifted
+#             to match the first image
+#           * The frame is updated so the next pair uses the stabilised frame
+#   4. Enhance Details and Denoise
+#   5. Write the video
+#
+#   Experiments:
+#
+#   References:
+#   [1] OpenCV 3.2.0 Documentation, 2016, [Online].
+#       Available: https://docs.opencv.org/3.2.0/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d.
+#       [Accessed: 2017-11-09]
+#   [2] M.Fang, GX.Yue1, QC.Yu, 'The Study on An Application of Otsu Method in Canny Operator',
+#       International Symposium on Information Processing, Huangshan, P. R. China,
+#       August 21-23, 2009, pp. 109-112
+#   [3] OpenCV 3.2.0 Documentation, 2016, [Online].
+#       Available: https://docs.opencv.org/3.2.0/d7/df3/group__imgproc__motion.html#ga552420a2ace9ef3fb053cd630fdb4952.
+#       [Accessed: 2017-11-09]
+#   [4] OpenCV 3.2.0 Tutorials, 2016, [Online].
+#       Available: https://docs.opencv.org/3.2.0/da/d6e/tutorial_py_geometric_transformations.html.
+#       [Accessed: 2017-11-09]
+
+
 
 import numpy as np
 import cv2
